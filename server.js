efda789bb9ae3eb9bff83594614f9f90e59424ac
madhurk <madhurk@in-madhurk.local>
2018-01-12 15:25:31 +0530

    Added logIn feature which will login when it is registered user.

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const http = require('http');
 const fs = require('fs');
 const PORT = 9099;
-let toS = o => JSON.stringify(o,null,2);
 const WebApp = require('./webapp');
+let registered_users =
+  [{userName:'madhurk',name:'Madhuri Kondekar'}]
 
 let app = WebApp.create();
-
 
 app.get('/',(req,res) =>{
   res.statusCode = 200;
@@ -28,11 +28,38 @@ app.get('/css/style.css',(req,res) =>{
   res.end();
 });
 
-app.post('/logInPage.html',(req,res) =>{
+app.get('/logInPage.html',(req,res) =>{
   res.statusCode = 200;
   res.setHeader('Content-type','text/html');
   res.write(fs.readFileSync('./public' + req.url));
   res.end();
+});
+
+app.get('/toDoList.html',(req,res) =>{
+  res.statusCode = 200;
+  res.setHeader('Content-type','text/html');
+  res.write(fs.readFileSync('./public' + req.url));
+  res.end();
+});
+
+app.post('/toDoList.html',(req,res) =>{
+  res.statusCode = 200;
+  res.setHeader('Content-type','text/html');
+  res.write(fs.readFileSync('./public' + req.url));
+  res.end();
+});
+
+app.post('/logInPage.html',(req,res) =>{
+  let user = registered_users.find(u => u.userName==req.body.name);
+  if(!user) {
+    res.setHeader('Set-Cookie',`logInFailed=true`);
+    res.redirect('/logInPage.html');
+    return;
+  }
+  let sessionid = new Date().getTime();
+  res.setHeader('Set-Cookie',`sessionid=${sessionid}`);
+  user.sessionid = sessionid;
+  res.redirect('/toDoList.html');
 });
 
 const server = http.createServer(app);
