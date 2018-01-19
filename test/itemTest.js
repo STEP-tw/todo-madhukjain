c9b030a765319c71c144b52bb297e8dc3b7b649c
veera83372 <veeravenky222@gmail.com>
2018-01-19 15:06:24 +0530

    Updated: Item model
        -Method and properties renamed.

--- test/itemTest.js
+++ test/itemTest.js
@@ -3,52 +3,54 @@ const assert = chai.assert;
 const Item = require('../lib/item.js');
 
 describe("Item",()=>{
-  describe("#getItemText",()=>{
-    it("Should return text of the item",()=>{
-      let item = new Item("Write test",1);
-      assert.equal(item.getText(),"Write test");
-    });
-  });
 
-  describe("#getStatus",()=>{
-    it("Should return status of the item for default",()=>{
-      let item = new Item("Write test",1);
-      assert.equal(item.getStatus(),false);
-    });
-    it("Should return status of the item for specified status",()=>{
-      let item = new Item("Write test",1,true);
-      assert.equal(item.getStatus(),true);
-    });
-  });
+   describe('accesing data',()=>{
+     describe("#getObjective",()=>{
+       it("should return objective of the item",()=>{
+         let item = new Item("Write test",1);
+         assert.equal(item.getObjective(),"Write test");
+       });
+     });
 
-  describe("#getId",()=>{
-    it("Should return status of the item for default",()=>{
-      let item = new Item("Write test",1);
-      assert.equal(item.getId(),1);
-    });
-  });
+     describe("#getStatus",()=>{
+       it("should return status of the item for default",()=>{
+         let item = new Item("Write test",1);
+         assert.equal(item.getStatus(),false);
+       });
+       it("should return status of the item for specified status",()=>{
+         let item = new Item("Write test",1,true);
+         assert.equal(item.getStatus(),true);
+       });
+     });
 
-  describe("#markAsDone",()=>{
-    it("Should set status as true",()=>{
-      let item = new Item("Write test",1);
-      item.markAsDone();
-      assert.equal(item.getStatus(),true);
-    });
-  });
+     describe("#getId",()=>{
+       it("should return id of the item",()=>{
+         let item = new Item("Write test",1);
+         assert.equal(item.getId(),1);
+       });
+     });
+   });
 
-  describe("#markAsNotDone",()=>{
-    it("Should set status as false",()=>{
-      let item = new Item("Write test",1,true);
-      item.markAsNotDone();
-      assert.equal(item.getStatus(),false);
-    });
-  });
+   describe('updating data',()=>{
+     describe("#updateStatus",()=>{
+       it("should set status as true",()=>{
+         let item = new Item("Write test",1);
+         item.updateStatus();
+         assert.equal(item.getStatus(),true);
+       });
+       it("should set status as false",()=>{
+         let item = new Item("Write test",1,true);
+         item.updateStatus();
+         assert.equal(item.getStatus(),false);
+       });
+     });
 
-  describe("#editText",()=>{
-    it("Should edit the item text",()=>{
-      let item = new Item("Write test",1,true);
-      item.editText("Read Book");
-      assert.equal(item.getText(),"Read Book");
-    });
-  });
+     describe("#updateObjective",()=>{
+       it("should update the item objective",()=>{
+         let item = new Item("Write test",1,true);
+         item.updateObjective("Read Book");
+         assert.equal(item.getObjective(),"Read Book");
+       });
+     });
+   });
 });
